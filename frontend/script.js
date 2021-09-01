414131561645758bc6bcc1db59b5a9f4dfc4634d
viczzhang <viczzhang@tencent.com>
2021-09-01 15:24:50 -0400

    website done

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,59 @@
-function upload(event) {
-    console.warn("It's taking awhile....");
+
+function handle_upload(event) {
+    // event.preventDefault();
+    const formData = new FormData(event.target);
+    const apigClient = apigClientFactory.newClient();
+
+    apigClient.uploadPost({},
+        {"email": formData.get("email"), "type": formData.get('file').type})
+        .then(function (response) {
+            upload_file_to_s3(response, formData)
+        })
+        .catch(function (error) {
+            console.log("Error", error)
+        });
+
+    alert("File uploaded! We'll email you when the job is done.");
+
 }
 
-const form = document.getElementById('uploader');
-form.addEventListener('submit', upload);
+function upload_file_to_s3(event, formData) {
+    const url = event['data']['presigned_url'];
+    const file = formData.get('file');
+
+    const http = new XMLHttpRequest();
+    http.open('PUT', url, false);
+    http.setRequestHeader('Content-type', file.type);
+    http.send(file);
+}
+
+function handle_download(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const apigClient = apigClientFactory.newClient();
+
+    apigClient.downloadFileidGet({"fileid": formData.get("file_id")})
+        .then(function (response) {
+            download_file_from_s3(response, formData)
+        })
+        .catch(function (error) {
+            console.log("Error", error)
+        });
+}
+
+function download_file_from_s3(event, formData) {
+    const http = new XMLHttpRequest();
+    http.open('GET', event['data']['presigned_url'], false);
+    http.send();
+    alert("File downloaded");
+}
+
+
+const upload = document.getElementById('uploader');
+upload.addEventListener('submit', handle_upload);
+
+
+const download = document.getElementById('downloader');
+download.addEventListener('submit', handle_download);
+
+
